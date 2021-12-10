1c5cb247d018e446d2b1e033c70beab8715d0755
ankahikahani <95933675+ankahikahani@users.noreply.github.com>
2021-12-10 21:44:41 +0100

    Update Class1.cs

--- _SharedSources1/Class1.cs
+++ _SharedSources1/Class1.cs
@@ -5,9 +5,13 @@ public class Class1
 	public Class1()
 	{
 
-		 void foo()
-        {
-			Console.WriteLine("Hello World! shared sources");
-		}
+		void foo()
+       			 {
+				Console.WriteLine("Hello World! shared sources");
+			}
+		void foo1()
+        		{
+				Console.WriteLine("Hello World! shared sources1 code");
+			}
 	}
 }

32c9bbfe7c9626cc94d0143bb54fec5725dc4eed
ankahikahani <95933675+ankahikahani@users.noreply.github.com>
2022-07-11 14:16:57 +0200

    Update Program.cs

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp2
             int y = Class2.foo(5, 6);
             Console.WriteLine("Hello World!2  "+y );
             Console.WriteLine("Hello World!3  "+y+2 );
+		int z = Class2.foo(5, 6);
         }
 		
 		void fun(){
